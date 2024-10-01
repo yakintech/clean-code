771eefcce48193adfc6778313e2214e9ca1ef6bf
cagatay.yildiz <cagatay@pixupplay.com>
2024-10-01 15:40:20 +0300

    comments, formatting, boundaries

--- 6-boundaries/index.js
+++ 6-boundaries/index.js
@@ -1,8 +1,22 @@
 //kodun sınırlarını belirlemek ve dış sistemlerle etkileşimini yönetmek için sınırlar belirleyin.
 
 
-function fetcUserData(id){
+//kötü örnek
+function fetchUserData(id){
     return fetch(`https://api.example.com/user/${id}`)
+    .then(response => response.json())
+    .then(data => data);
+}
+
+
+//iyi örnek
+function fetchPersonData(id){
+    const url = `https://api.example.com/person/${id}`;
+    return fetchFromApi(url);
+}
+
+function fetchFromApi(url){
+    return fetch(url)
     .then(response => response.json())
     .then(data => data);
 }
